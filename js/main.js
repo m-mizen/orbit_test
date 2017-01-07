9338e6fb8f788432a3bc8e7ae8989b1c94e98b46
Mark Mizen <mark@redballoondesign.co.uk>
2017-01-07 13:00:51 +0000

    overhaul of system

--- js/main.js
+++ js/main.js
@@ -1,28 +1,10 @@
-// This info would be brought in from somewhere else. Maybe stored in a database or an external json file.
-var systemInfo = {
-    systemName: "Test system",
-    star: {
-        type: "blue giant"
-    },
-    planets: [{
-            name: "test planet 1",
-            colour: "red",
-            size: 8,
-            orbit: {
-                a: 200, // semi-major axis
-                e: 0.1, //eccentricity  
-            }
-        },
-        {
-            name: "test planet 2",
-            colour: "blue",
-            size: 5,
-            orbit: {
-                a: 400, // semi-major axis
-                e: 0.01, //eccentricity  
-            }
-        }
-    ]
+// Get the system info:
+function getSystem(systemName) {
+    var xmlHttp = new XMLHttpRequest();
+    var theUrl = "universe/" + systemName + ".json";
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return JSON.parse(xmlHttp.responseText);
 };
 
 // Time variables
@@ -30,6 +12,8 @@ var t, timeDisplay;
 
 // Init function
 function init() {
+    console.log(getSystem('system1'));
+
     t = 0;
     timeDisplay = document.getElementById("time");
 
@@ -45,40 +29,19 @@ function init() {
 
 
     // Create sun
-    var sun = new star();
-    sun.addToStage(stage);
+
 
     // Create planets
-    var planets = [];
-    for (let i = 0; i < systemInfo.planets.length; i++) {
-        let thisPlanetInfo = systemInfo.planets[i];
-        planets[i] = new oribitalBody({
-                size: thisPlanetInfo.size,
-                colour: thisPlanetInfo.colour,
-            },
-            systemInfo.planets[i].orbit
-        );
-        planets[i].addToStage(sun.system);
-        planets[i].checkValues();
-    }
 
 
     // Start update loop
     createjs.Ticker.addEventListener("tick", handleTick);
-
-    var scaleSystem = 1;
 
     function handleTick() {
         // Add to time variable
         t++;
         // update time display
         timeDisplay.innerHTML = t;
-
-        // Zoom out on page
-        scaleSystem = 1 - ((t % 80) / 100);
-
-        sun.system.scaleX = scaleSystem
-        sun.system.scaleY = scaleSystem
 
         stage.update();
     }
--- js/source/classes.js
+++ js/source/classes.js
@@ -1,92 +1,82 @@
-class star {
-    /**
-     * Class for stars
-     */
-    constructor() {
-        this.circle = new createjs.Shape();
+class spacialBody {
 
-        this.system = new createjs.Container();
+    constructor(parent) {
 
-        this.size = 10;
+        // The body this object is in orbit around
+        this.parent;
+
+        // The cartisian co-ordinates of the body
         this.position;
-        this.brightness = 100;
-    }
-    addToStage(thisStage) {
-        this.position = {
-            x: -this.size / 2,
-            y: -this.size / 2
-        }
-        this.system.x = thisStage.canvas.width / 2;
-        this.system.y = (thisStage.canvas.height / 2);
 
-        this.circle.graphics.beginFill("yellow").drawCircle(this.position.x, this.position.y, this.size);
-        thisStage.addChild(this.system);
-        this.system.addChild(this.circle);
+        // The circle that appears around the object at all zoom levels;
+        this.uiCircle;
+
+        // The actual planet image
+        this.bodyCircle;
+
+        // Attributes for the planet
+        this.attributes;
+
+        // A container that objects which orbit this body reside within.
+        this.influence;
+
+    }
+
+    buildBody(time) {
+
+    }
+
+    updateBody(time) {
+
+    }
+
+    bodyInfo(attribute) {
+        if (typeof attribute === string) {
+            if (typeof this[attribute] !== "undefined") {
+                return this[attribute];
+            } else {
+                return attribute + " doesn't exist";
+            }
+        } else {
+            return this.attributes;
+        }
+    }
+
+}
+
+class star extends spacialBody {
+    constructor() {
+        super();
     }
 }
 
-class oribitalBody {
-    /**
-     * Class for obital bodies (planets moons etc)
-     */
-    constructor(info, orbitInfo) {
-
-        this.circle = new createjs.Shape(); // Planet circle
-        this.displayOrbit = new createjs.Shape(); // Orbit ellipse
-
-        this.size = 10; // default size
-        this.colour = "red"; // default colour
-
-        this.orbit = orbitInfo; // import orbit info
-
-        this.currentLocation = {
-            x: -(this.orbit.a),
-            y: 0
-        }; // Set start location
-
-        if (typeof info === "object") {
-            this.size = info.size || this.size;
-            this.colour = info.colour || this.colour;
-        }
+class orbitalBody extends spacialBody {
+    constructor() {
+        super();
     }
-    addToStage(thisContainer) {
-        // Add the planet and the orbit path to the container.
+    gravity() {}
+}
 
-        // Update the position of the planet.
-        this.updatePosition(window.t);
-
-        // Draw Planet
-        this.circle.graphics.beginFill(this.colour).drawCircle(this.currentLocation.x, this.currentLocation.y, this.size)
-
-        // Calc orbit ellipse
-        let rmax = this.orbit.a;
-        let rmin = this.orbit.a * (1 - this.orbit.e);
-        let orbitWidth = rmax + rmin;
-        let orbitHeight = 2 * Math.sqrt(rmax * rmin);
-
-        // Draw orbit ellipse
-        this.displayOrbit.graphics.beginStroke('purple').drawEllipse(
-            (-rmax),
-            (orbitHeight / -2),
-            orbitWidth,
-            orbitHeight
-        );
-
-        // Add shapes to parent
-        thisContainer.addChild(this.circle);
-        thisContainer.addChild(this.displayOrbit);
-
+class moon extends orbitalBody {
+    constructor() {
+        super();
     }
-    updatePosition(time) {
-        // Update location of the planet
+}
+
+class asteroid extends orbitalBody {
+    constructor() {
+        super();
     }
-    checkValues() {
-        // For debugging
-        console.log(this);
-        let rmax = this.orbit.a;
-        let rmin = this.orbit.a * (1 - this.orbit.e);
-        let orbitWidth = rmax + rmin;
-        let orbitHeight = Math.sqrt(rmax * rmin);
-        console.log(rmax, rmin, orbitHeight, orbitWidth);
+}
+
+class station extends orbitalBody {
+    constructor() {
+        super();
+    }
+}
+
+class ship extends orbitalBody {
+    constructor() {
+        super();
     }
 }
