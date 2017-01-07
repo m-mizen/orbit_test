8f7ccfbddd7a0736f47a29cf0f8677d86f6908f9
Mark Mizen <mark@redballoondesign.co.uk>
2017-01-07 23:06:47 +0000

    massive updates to classes and values

--- js/source/classes.js
+++ js/source/classes.js
@@ -1,33 +1,74 @@
-class spacialBody {
+class SpacialBody {
 
-    constructor(parent) {
+    constructor(options, parent) {
 
         // The body this object is in orbit around
-        this.parent;
+        this.parent = parent;
 
         // The cartisian co-ordinates of the body
-        this.position;
+        this.position = {
+            x: 0,
+            y: 0
+        };
 
         // The circle that appears around the object at all zoom levels;
         this.uiCircle;
+        this.uiCircleSize = 12;
 
-        // The actual planet image
+        // The actual body image (real size)
         this.bodyCircle;
 
         // Attributes for the planet
-        this.attributes;
+        this.attributes = {
+            scale: Reality.scale,
+            mass: options.mass,
+            colour: options.colour
+        };
 
         // A container that objects which orbit this body reside within.
         this.influence;
 
+        this.influenceRange;
+
+        this.velocity = new Vector(0, 0);
+
     }
 
     buildBody(time) {
+        // Build the body. Run when building the system
+
+        this.influence = new createjs.Container();
+        this.influence.x = this.position.x;
+        this.influence.y = this.position.y;
+        this.influence.mass = this.attributes.mass;
+
+        this.parent.addChild(this.influence);
+
+
+        this.uiCircle = new createjs.Shape();
+        this.uiCircle.graphics.beginStroke("white").drawCircle(
+            ((-this.position.x) * Reality.scale),
+            ((-this.position.y) * Reality.scale),
+            this.uiCircleSize
+        );
+
+        this.parent.addChild(this.uiCircle);
+
+        this.bodyCircle = new createjs.Shape();
+
+        this.bodyCircle.x = (this.position.x * Reality.scale);
+        this.bodyCircle.y = (this.position.y * Reality.scale);
+        this.bodyCircle.graphics.beginFill(this.attributes.colour).drawCircle(
+            ((-this.position.x) * Reality.scale),
+            ((-this.position.y) * Reality.scale),
+            Reality.scale * this.attributes.size
+        );
+        this.parent.addChild(this.bodyCircle);
 
     }
 
     updateBody(time) {
-
+        // Update the body. Every time something interacts with the body that is dependant upon time this function is called.
     }
 
     bodyInfo(attribute) {
@@ -42,41 +83,122 @@ class spacialBody {
         }
     }
 
+    get speed() {
+        return this.velocity.length;
+    }
+
+    get distaceToParent() {
+        return ((this.position.x + this.position.y) / 2);
+    }
+
 }
 
-class star extends spacialBody {
-    constructor() {
-        super();
+class Star extends SpacialBody {
+    constructor(options, parent) {
+        super(options, parent);
     }
 }
 
-class orbitalBody extends spacialBody {
-    constructor() {
-        super();
+class OrbitalBody extends SpacialBody {
+    constructor(options, parent) {
+        super(options, parent);
+
+        this.orbitinfo = {
+            a: parseInt(options.orbit.a) * Reality.AU,
+            e: options.orbit.e,
+            w: options.orbit.w,
+            t: 0,
+            t0: parseFloat(options.orbit.start),
+            force: 0,
+            period: 0
+        };
+        this.orbitinfo.force = Reality.G * this.parent.mass / this.orbitinfo.a
+        this.orbitinfo.period = (2 * Math.PI) * Math.sqrt(((this.orbitinfo.a * 1000) ** 3) / this.orbitinfo.force);
+        this.orbitinfo.t0 = this.orbitinfo.t0 * this.orbitinfo.period;
+
+        this.orbitPath;
+        this.attributes.name = options.name;
+
     }
-    gravity() {}
+    buildBody(time) {
+        time = time || 0;
+        // Build the body. Run when building the system
+        this.updateOrbit(time);
+
+        this.influence = new createjs.Container();
+        this.influence.x = this.position.x * Reality.scale;
+        this.influence.y = this.position.y * Reality.scale;
+
+        this.uiCircle = new createjs.Shape();
+        this.uiCircle.graphics.beginStroke("white").drawCircle(
+            ((this.position.x) * Reality.scale),
+            ((this.position.y) * Reality.scale),
+            this.uiCircleSize
+        );
+        this.parent.addChild(this.uiCircle);
+
+        this.bodyCircle = new createjs.Shape();
+        this.bodyCircle.x = (this.position.x * Reality.scale);
+        this.bodyCircle.y = (this.position.y * Reality.scale);
+        this.bodyCircle.graphics.beginFill(this.attributes.colour).drawCircle(
+            ((this.position.x) * Reality.scale),
+            ((this.position.y) * Reality.scale),
+            Reality.scale * this.attributes.size
+        );
+        this.parent.addChild(this.bodyCircle);
+
+        this.orbitPath = new createjs.Shape();
+        this.orbitPath.graphics.beginStroke("white").drawCircle(0, 0, this.orbitinfo.a * Reality.scale);
+        this.parent.addChild(this.orbitPath);
+    }
+
+    updateBody(time) {
+        time = time || 0;
+        // Update the body. Every time something interacts with the body that is dependant upon time this function is called.
+        this.updateOrbit(time);
+
+        this.influence.x = this.uiCircle.x = this.bodyCircle.x = this.position.x * Reality.scale - this.orbitinfo.a * Reality.scale;
+        // this.influence.x = this.uiCircle.x = this.bodyCircle.x = this.position.x * Reality.scale;
+        this.influence.y = this.uiCircle.y = this.bodyCircle.y = this.position.y * Reality.scale;
+
+    }
+
+    updateOrbit(time) {
+        time = time || 0;
+        // Calculate the current orbit location and velocity
+        this.orbitinfo.t = time;
+        this.position.x = this.orbitinfo.a * Math.cos((2 * Math.PI) * (this.orbitinfo.t + this.orbitinfo.t0) / this.orbitinfo.period);
+        this.position.y = this.orbitinfo.a * Math.sin((2 * Math.PI) * (this.orbitinfo.t + this.orbitinfo.t0) / this.orbitinfo.period);
+
+    }
 }
 
-class moon extends orbitalBody {
-    constructor() {
-        super();
+class Moon extends OrbitalBody {
+    constructor(options, parent) {
+        super(options, parent);
     }
 }
 
-class asteroid extends orbitalBody {
-    constructor() {
-        super();
+class Asteroid extends OrbitalBody {
+    constructor(options, parent) {
+        super(options, parent);
     }
 }
 
-class station extends orbitalBody {
-    constructor() {
-        super();
+class Station extends OrbitalBody {
+    constructor(options, parent) {
+        super(options, parent);
     }
 }
 
-class ship extends orbitalBody {
-    constructor() {
-        super();
+class Ship extends OrbitalBody {
+    constructor(options, parent) {
+        super(options, parent);
+    }
+}
+
+class PlayerShip extends Ship {
+    constructor(options, parent) {
+        super(options, parent);
     }
 }
--- js/source/physics.js
+++ js/source/physics.js
@@ -1,4 +1,17 @@
-var Physics = function() {
+var RealityClass = function() {
     this.G = (6.67408 / (10 ** -11));
-    this.scale = 1 / 1000000;
+    this.scale = 1 / 1000000000;
+    // this.scale = 1;
+    this.AU = 149600000000;
+    this.timeConst = 1000;
+    this.width = 1200;
+    this.height = 800;
+    this.gravityForce = function(m1, m2, r) {
+        var force = this.G * (m1 * m2) / r;
+        return force;
+    }
+    this.timeC = 100;
 }
+var Reality = new RealityClass();
+
+var scaleValues = [(1 / 1000000000), (1 / 1000000000), (1 / 1000000000)];
